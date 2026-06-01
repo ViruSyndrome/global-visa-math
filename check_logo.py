37098bbca523a32b18b6307030139a53d3c0114c
ViruSyndrome <vinodjamesisaac@gmail.com>
2026-06-01 17:22:27 +0530

    Remove debug/patch scripts (check_logo, inject_seo, patch_* files)

--- check_logo.py
+++
@@ -1,9 +0,0 @@
-import glob
-
-results = {}
-for f in glob.glob('*.html'):
-    with open(f, 'r', encoding='utf-8') as file:
-        content = file.read()
-        results[f] = 'class="logo"' in content
-
-print(results)
